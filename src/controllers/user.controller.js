b49f9dfde7fec332c02afdccaf8b0109b3a9193c
Nirav2283 <niravvaghasia95@gmail.com>
2024-11-21 20:32:34 +0530

    Fetch WatchHistory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import mongoose from 'mongoose';
 import {ApiError} from'../utils/ApiError.js';
 import { User } from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
@@ -126,7 +127,7 @@ const logoutUser = asyncHandler(async(req, res) => {
         req.user._id,
         {
             $unset: {
-                refreshToken: 1 // this removes the field from document
+                refreshToken: 1 
             }
         },
         {
@@ -245,8 +246,186 @@ const updateAccountDetails =asyncHandler(async(req, res)=>{
     
 })
 
+const updateUserAvatar= asyncHandler(async(req, res)=>{
+    const avatarLocalPath=req.file?.path
 
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is missing")
+    }
 
+    const avatar= await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400, 'Error while uploading on avatar')
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    
+    return res.status(200)
+              .json(new ApiResponse(200, user, "avatar updated successfully"))
+})
+
+const updateUserCoverImage= asyncHandler(async(req, res)=>{
+    const coverImageLocalPath=req.file?.path
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "Cover Image file is missing")
+    }
+
+    const coverImage= await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage.url){
+        throw new ApiError(400, "Error while uploading on coverImage")
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res.status(200)
+              .json(new ApiResponse(200, user, "cover image updated successfully"))
+})
+
+const getUserChannelProfile= asyncHandler(async(req, res)=>{
+    const {username}=req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400, "username is missing")
+    }
+
+    const channel=await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {
+                            $in: [req.user?._id, "$subscribers.subscriber"]
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(400, "channel does not exists")
+    }
+
+    return res.status(200)
+              .json(
+                new ApiResponse(200, channel[0], "user channel fetched successfully")
+              )
+
+})
+
+const getWatchHistory= asyncHandler(async(req, res)=>{
+    const user= await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200)
+              .json(
+                new ApiResponse(
+                    200, user[0].watchHistory,
+                    "watch history fetched successfully"
+                )
+               )
+})
 
 
 export { registerUser , 
@@ -255,5 +434,9 @@ export { registerUser ,
     refreshAccessToken ,
     changeCurrentPassword ,
     getCurrentUser , 
-    updateAccountDetails
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
